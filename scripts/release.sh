8b29fd9739a0d84d8e6035d0ac26bd8631a69495
noituri <mikolaj.radkowski@protonmail.com>
2025-02-26 13:09:55 +0100

    add latest tag

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,13 +5,13 @@ set -euo pipefail
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 
 set +u
-if [[ -z "$WORKFLOW_RUN_ID" ]]; then
-  echo "WORKFLOW_RUN_ID env variable is required. You can list recent runs using gh run list --workflow \"package for release\" command."
-  echo ""
-  echo "Recent workflow runs:"
-  gh run list --workflow "package for release" | cat
-  exit 1
-fi
+# if [[ -z "$WORKFLOW_RUN_ID" ]]; then
+#   echo "WORKFLOW_RUN_ID env variable is required. You can list recent runs using gh run list --workflow \"package for release\" command."
+#   echo ""
+#   echo "Recent workflow runs:"
+#   gh run list --workflow "package for release" | cat
+#   exit 1
+# fi
 
 if [[ -z "$RELEASE_TAG" ]]; then
   echo "RELEASE_TAG env variable is required."
@@ -35,28 +35,29 @@ fi
 
 set -u
 
-mkdir -p "$ROOT_DIR/release_tmp"
-cd "$ROOT_DIR/release_tmp"
+# mkdir -p "$ROOT_DIR/release_tmp"
+# cd "$ROOT_DIR/release_tmp"
 
-gh run download "$WORKFLOW_RUN_ID" -n smelter_linux_x86_64.tar.gz
-gh run download "$WORKFLOW_RUN_ID" -n smelter_linux_aarch64.tar.gz
-gh run download "$WORKFLOW_RUN_ID" -n smelter_darwin_x86_64.tar.gz
-gh run download "$WORKFLOW_RUN_ID" -n smelter_darwin_aarch64.tar.gz
-gh run download "$WORKFLOW_RUN_ID" -n smelter_with_web_renderer_linux_x86_64.tar.gz
-gh run download "$WORKFLOW_RUN_ID" -n smelter_with_web_renderer_darwin_x86_64.tar.gz
-gh run download "$WORKFLOW_RUN_ID" -n smelter_with_web_renderer_darwin_aarch64.tar.gz
+# gh run download "$WORKFLOW_RUN_ID" -n smelter_linux_x86_64.tar.gz
+# gh run download "$WORKFLOW_RUN_ID" -n smelter_linux_aarch64.tar.gz
+# gh run download "$WORKFLOW_RUN_ID" -n smelter_darwin_x86_64.tar.gz
+# gh run download "$WORKFLOW_RUN_ID" -n smelter_darwin_aarch64.tar.gz
+# gh run download "$WORKFLOW_RUN_ID" -n smelter_with_web_renderer_linux_x86_64.tar.gz
+# gh run download "$WORKFLOW_RUN_ID" -n smelter_with_web_renderer_darwin_x86_64.tar.gz
+# gh run download "$WORKFLOW_RUN_ID" -n smelter_with_web_renderer_darwin_aarch64.tar.gz
 
 IMAGE_NAME="ghcr.io/noituri/smelter"
 docker buildx imagetools create -t "${IMAGE_NAME}:${RELEASE_TAG}" "${IMAGE_NAME}:${COMMIT_HASH}"
+docker buildx imagetools create -t "${IMAGE_NAME}:latest" "${IMAGE_NAME}:${COMMIT_HASH}"
 docker buildx imagetools create -t "${IMAGE_NAME}:${RELEASE_TAG}-web-renderer" "${IMAGE_NAME}:${COMMIT_HASH}-web-renderer"
 
-gh release create "$RELEASE_TAG"
-gh release upload "$RELEASE_TAG" smelter_linux_x86_64.tar.gz
-gh release upload "$RELEASE_TAG" smelter_linux_aarch64.tar.gz
-gh release upload "$RELEASE_TAG" smelter_darwin_x86_64.tar.gz
-gh release upload "$RELEASE_TAG" smelter_darwin_aarch64.tar.gz
-gh release upload "$RELEASE_TAG" smelter_with_web_renderer_linux_x86_64.tar.gz
-gh release upload "$RELEASE_TAG" smelter_with_web_renderer_darwin_x86_64.tar.gz
-gh release upload "$RELEASE_TAG" smelter_with_web_renderer_darwin_aarch64.tar.gz
+# gh release create "$RELEASE_TAG"
+# gh release upload "$RELEASE_TAG" smelter_linux_x86_64.tar.gz
+# gh release upload "$RELEASE_TAG" smelter_linux_aarch64.tar.gz
+# gh release upload "$RELEASE_TAG" smelter_darwin_x86_64.tar.gz
+# gh release upload "$RELEASE_TAG" smelter_darwin_aarch64.tar.gz
+# gh release upload "$RELEASE_TAG" smelter_with_web_renderer_linux_x86_64.tar.gz
+# gh release upload "$RELEASE_TAG" smelter_with_web_renderer_darwin_x86_64.tar.gz
+# gh release upload "$RELEASE_TAG" smelter_with_web_renderer_darwin_aarch64.tar.gz
 
-rm -rf "$ROOT_DIR/release_tmp"
+# rm -rf "$ROOT_DIR/release_tmp"
